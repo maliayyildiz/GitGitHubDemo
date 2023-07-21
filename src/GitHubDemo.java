5d882578dbb6b6b6d1b9efc261860adf4be32ac0
maliayyildiz <maliayyiliz61@gmail.com>
2023-07-21 23:43:56 +0200

    yeni kod eklendi

--- src/GitHubDemo.java
+++
@@ -1,3 +0,0 @@
-public class GitHubDemo {
-    // yeni kod eklendi
-}
--- src/UdemyGit.java
+++
@@ -1,5 +0,0 @@
-public class UdemyGit {
-    //Udemy kod
-    // yeni branch acarak kodalrimizi ana projeye eklemek istiyoruz
-    //takim arkadasimiz kod ekledi
-}
