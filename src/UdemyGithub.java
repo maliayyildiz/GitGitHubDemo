a7c8f7d92f80daef77b9448246be73836317e58e
maliayyildiz <maliayyiliz61@gmail.com>
2023-07-22 00:28:40 +0200

    git komutlari eklendi

--- src/UdemyGithub.java
+++ src/UdemyGithub.java
@@ -1,3 +1,13 @@
 public class UdemyGithub {
     //yeni bir kod eklendi
 }
+//        git init: Bir klasörü Git deposu olarak başlatmak için kullanılır. Bu komut, bir klasörde yeni bir Git deposu oluşturur.
+//        git add <dosya>: Değişiklikleri bir sonraki taahhüte eklemek için kullanılır. <dosya>, taahhüde dahil etmek istediğiniz dosya veya dosyaların adını temsil eder. Tüm değişiklikleri eklemek için git add . kullanabilirsiniz.
+//        git commit -m "<açıklama>": Yapılan değişiklikleri bir taahhüt olarak kaydetmek için kullanılır. <açıklama>, taahhütle ilgili açıklamayı temsil eder.
+//        git push: Yerel taahhütleri uzak bir depoya göndermek için kullanılır. Bu komut, yerel taahhütleri uzak sunucuya yükler ve güncelleştirir.
+//        git pull: Uzaktaki depodaki güncel değişiklikleri almak ve yerel kopyayı güncellemek için kullanılır. Bu komut, git fetch ve ardından git merge komutlarını birleştirir.
+//        git branch: Mevcut dalları listelemek için kullanılır. Aktif dalı (* işareti ile gösterilir) ve diğer mevcut dalları görüntüler.
+//        git checkout <dal_adı>: Bir dala geçmek için kullanılır. <dal_adı>, geçmek istediğiniz dalın adını temsil eder. Bu komut, o dalda yapacağınız değişiklikleri etkileyecektir.
+//        git merge <dal_adı>: Aktif dalı belirtilen dal ile birleştirmek için kullanılır. Bu komut, iki dalı birleştirerek değişiklikleri tek bir dalda birleştirir.
+//        git status: Depodaki değişikliklerin durumunu ve takip edilen dosyaların durumunu gösterir. Hangi dosyaların değiştirildiğini, hangi dosyaların eklenip çıkarıldığını ve hangi dosyaların taahhüt edileceğini görmenizi sağlar.
+//        git log: Taahhüt geçmişini gösterir. Hangi taahhütlerin yapıldığını, kimin yaptığını, ne zaman yapıldığını ve açıklamaları görüntüler.
