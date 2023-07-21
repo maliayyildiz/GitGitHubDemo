4d4f9c8a692aa7d49591cd339dc2802ba4eb343e
maliayyildiz <maliayyiliz61@gmail.com>
2023-07-21 11:23:10 +0200

    3. commit

--- src/Ornek.java
+++ src/Ornek.java
@@ -1,3 +1,7 @@
 public class Ornek {
     //2. ekleme
+    //ilk once kodlamamiz bittiyse git dosyasina ekliyoruz
+    // git add <filePath> specific dosya
+    // git add . ". yapilan butun degisiklikleri ekler"
+
 }
